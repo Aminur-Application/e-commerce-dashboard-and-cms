d008658c957dbe16904557b886661df98b93473e
Prophecyuntold <aminur999@hotmail.com>
2023-07-28 22:43:02 +0100

    install shadcn for simple tailwind component components

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <div className="p-3"> 
+      <p> Admin dashboard </p>
+
+      <Button>Click Me</Button>
+   </div>
+   
+  )
+}
