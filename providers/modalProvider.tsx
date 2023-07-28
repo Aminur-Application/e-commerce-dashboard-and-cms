82415a5963384b4b066938fb5d94b2e19b87edf9
Prophecyuntold <aminur999@hotmail.com>
2023-07-28 23:27:16 +0100

    create modalProvider to avoid hydration issue

---
+++ providers/modalProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import StoreModal from '@/components/ui/modals/storeModal';
+import React, { useEffect, useState } from 'react'
+
+const modalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if(!isMounted){
+    return null;
+  }
+  
+  return (
+    <>
+      <StoreModal /> 
+    </>
+  )
+}
+
+export default modalProvider
