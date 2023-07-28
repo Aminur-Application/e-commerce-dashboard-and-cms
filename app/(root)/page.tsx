4cdc09cf1dc96bbd60ace43f46b47c7d1ef14415
Prophecyuntold <aminur999@hotmail.com>
2023-07-28 23:27:46 +0100

    display modal on root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,22 @@
 "use client"
+
 import { Modal } from "@/components/ui/modal"
+import { useStoreModal } from "@/hooks/useStoreModal"
 import { UserButton } from "@clerk/nextjs"
+import { useEffect } from "react"
 
 const RootPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if(!isOpen){
+      onOpen()
+    }
+  }, [onOpen, isOpen])
+  
   return (
    <div className="p-3"> 
-      <Modal title="test" description="test" isOpen onClose={() => {}}>
-        children
-      </Modal>
       <p> Admin dashboard. This is a protected page</p>
    </div>
    
