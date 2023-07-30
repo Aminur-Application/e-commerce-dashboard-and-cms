1cbfbfc77f95a87f4af0f30d6892423db4f1265d
Prophecyuntold <aminur999@hotmail.com>
2023-07-30 13:53:06 +0100

    build navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,34 @@
+import { UserButton, auth } from '@clerk/nextjs'
+import React from 'react'
+import MainNav from '@/components/mainNav'
+import StoreSwitcher from '@/components/storeSwitcher'
+import { redirect } from 'next/navigation'
+import prismadb from '@/lib/prismadb'
+
+const navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    }
+  });
+  
+  return (
+    <div className='border-b'>
+      <div className='flex h-16 items-center px-4'>
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6"/>
+        <div className='ml-auto flex items-center space-x-4'>
+          <UserButton afterSignOutUrl='/'/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default navbar
