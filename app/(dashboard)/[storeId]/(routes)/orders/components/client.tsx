8127bd068e03de658ee399d007f73e1077bb2e30
Prophecyuntold <Aminurr999@gmail.com>
2023-10-05 22:42:54 +0100

    order items completes

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import React from "react";
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/dataTable";
+import ApiList from "@/components/ui/apiList";
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="products" />
+    </>
+  );
+};
+
+export default OrderClient;
