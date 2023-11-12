0f0312d69f81c46a975264ecb31ff1e01489146c
Prophecyuntold <Aminurr999@gmail.com>
2023-11-12 14:36:16 +0000

    dark mode feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,39 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-import ModalProvider from '@/providers/modalProvider'
-import { ToasterProvider } from '@/providers/toastProvider'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import ModalProvider from "@/providers/modalProvider";
+import { ToasterProvider } from "@/providers/toastProvider";
+import { ThemeProvider } from "@/providers/themeProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Admin dashboard',
-  description: 'Admin dashboard',
-}
+  title: "Admin dashboard",
+  description: "Admin dashboard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToasterProvider />
-          <ModalProvider />
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,34 +1,36 @@
-import { UserButton, auth } from '@clerk/nextjs'
-import React from 'react'
-import MainNav from '@/components/mainNav'
-import StoreSwitcher from '@/components/storeSwitcher'
-import { redirect } from 'next/navigation'
-import prismadb from '@/lib/prismadb'
+import { UserButton, auth } from "@clerk/nextjs";
+import React from "react";
+import MainNav from "@/components/mainNav";
+import StoreSwitcher from "@/components/storeSwitcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "./themeToggle";
 
 const navbar = async () => {
   const { userId } = auth();
 
   if (!userId) {
-    redirect('/sign-in');
+    redirect("/sign-in");
   }
 
   const stores = await prismadb.store.findMany({
     where: {
       userId,
-    }
+    },
   });
-  
+
   return (
-    <div className='border-b'>
-      <div className='flex h-16 items-center px-4'>
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
         <StoreSwitcher items={stores} />
-        <MainNav className="mx-6"/>
-        <div className='ml-auto flex items-center space-x-4'>
-          <UserButton afterSignOutUrl='/'/>
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
